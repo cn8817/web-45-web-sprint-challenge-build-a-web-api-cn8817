420836f744b052c64bbe3a13427ed87d3dc9cab6
cn8817 <christinenguyen1433@gmail.com>
2021-09-02 16:10:54 -0700

    made get endpoints for action router

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,5 +1,21 @@
 // Write your "actions" router here!
 const express = require('express')
+const { validateProjectById } = require('../projects/projects-middleware')
+const { validateActionId, validateAction } = require('./actions-middlware')
+
+const Action = require('./actions-model')
 const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    Action.get()
+        .then(actions => {
+            res.status(200).json(actions)
+        })
+        .catch(next)
+})
+
+router.get('/:id', validateActionId, (req, res) => {
+    res.status(200).json(req.action)
+})
 
 module.exports = router
