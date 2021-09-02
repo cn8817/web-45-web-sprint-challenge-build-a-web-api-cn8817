1a315bd68d1de6693e4008017fc7b53c8bebd052
cn8817 <christinenguyen1433@gmail.com>
2021-09-02 15:56:28 -0700

    completed projects delete endpoint and made actions middleware

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,35 @@
 // add middlewares here related to actions
+const Action = require('./actions-model')
+
+async function validateActionId(req, res, next) {
+    try {
+        const action = await Action.get(req.params.id)
+        if(!action) {
+            res.status(404).json({
+                message: 'action not found'
+            })
+        } else{
+            req.action = action
+            next()
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'problem finding action'
+        })
+    }
+}
+
+function validateAction(req, res, next) {
+    const { description, notes } = req.body
+    if(!description || !description.trim() || !notes || !notes.trim()) {
+        res.status(400).json({
+            message: 'missing description or notes'
+        })
+    } else{
+        req.description = description.trim()
+        req.notes = notes.trim()
+        next()
+    }
+}
+
+module.exports = { validateActionId, validateAction }
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -18,11 +18,39 @@ router.get('/:id', validateProjectById, (req, res) => {
 })
 
 router.post('/', validateProject, (req, res, next) => {
-    Project.insert({ name: req.name, description: req.description})
+    Project.insert({ 
+        name: req.name, 
+        description: req.description})
         .then(newProject => {
             res.status(201).json(newProject)
         })
         .catch(next)
+})
+
+router.put('/:id', validateProjectById, validateProject, (req, res, next) => {
+    Project.update(req.params.id, { name: req.name, description: req.description})
+        .then(projects => {
+            res.status(200).json(projects)
+        })
+        .catch(next)
+})
+
+router.delete('/:id', validateProjectById, async (req, res, next) => {
+    try {
+        await Project.remove(req.params.id)
+        res.status(200).json(req.project)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.get('/:id/actions', validateProjectById, async (req, res, next) => {
+    try {
+        const actions = await Project.getProjectActions(req.params.id)
+        res.status(200).json(actions)
+    } catch(err) {
+        next(err)
+    }
 })
 
 module.exports = router
