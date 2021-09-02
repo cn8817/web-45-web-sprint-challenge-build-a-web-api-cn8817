cae0f129d8a72e8bb27deeaa39dfd44a76f0e5b0
cn8817 <christinenguyen1433@gmail.com>
2021-09-02 15:21:12 -0700

    made projects middleware, added server to index and created server

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,36 @@
 // add middlewares here related to projects
+const Project = require('./projects-model'
+)
+
+async function validateProjectById(req, res, next) {
+    try{
+        const project = await Project.get(req.params.id)
+        if(!project) {
+            res.status(404).json({
+                message: 'project not found'
+            })
+        } else{
+            req.project = project
+            next()
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'problem finding the post'
+        })
+    }
+}
+
+function validateProject(req, res, next) {
+    const { name, description } = req.body
+    if(!name || !name.trim() || !description || !description.trim()) {
+        res.status(400).json({
+            message: 'missing either name or description'
+        })
+    } else {
+        req.name = name.trim()
+        req.description = description.trim()
+        next()
+    }
+}
+
+module.exports = { validateProject, validateProjectById }
